1fe3578d3b2e781fe1b841f8f50308299631a549
nRegtLu <1494185212@qq.com>
2024-04-27 22:12:55 +0800

    优化播放

--- play.py
+++ play.py
@@ -3,31 +3,37 @@ import sys
 import time
 import playsound
 
+def 内容提供(缓存路径:str):
+    结束=False
+    索引=0
+    while not 结束:
+        索引+=1
+        try:
+            yield open(os.path.join(f'{缓存路径}/ascii', str(索引+1)+'.txt'), 'r', encoding='utf8').read().strip('\n')
+        except:
+            结束=True
+    yield False
+    return
 
 def play(帧率:int,缓存路径:str,音频路径:str):
-    
-    帧数=0
-
-    for root, dirs, files in os.walk(f'{缓存路径}/ascii'):
-        for file in files:
-            if file.endswith('.txt'):
-                帧数+=1
-
     时间间隔 = 1 / 帧率
+    当前帧=0
     os.system('cls')
     sys.stdout.write('\033[25l')
     开始时间 = time.time()
-
+    内容提供器=内容提供(缓存路径)
     playsound.playsound(f'{缓存路径}/{音频路径}',False)
-    for file in range(帧数):
+    while True:
+        当前帧+=1
         当前时间=time.time()
-        预期时间=开始时间+时间间隔*file
+        预期时间=开始时间+时间间隔*当前帧
+        内容=next(内容提供器)
         if 预期时间<当前时间:
             continue
         if 预期时间>当前时间:
             time.sleep(预期时间-当前时间)
-        with open(os.path.join(f'{缓存路径}/ascii', str(file+1)+'.txt'), 'r', encoding='utf8') as f:
-            sys.stdout.write(f.read().strip('\n'))
+        if not 内容:
+            break
+        sys.stdout.write(内容)
         sys.stdout.write('\033[H')
-
     os.system('cls')
